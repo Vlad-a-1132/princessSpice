e87615d3790006504495b2b47c557e9296b6c078
Vlad-a-1132 <vladfrolov757@gmail.com>
2024-07-26 09:28:17 +0300

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: "standalone",
+	
+	reactStrictMode: false,
+
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+};
+
+export default nextConfig;
